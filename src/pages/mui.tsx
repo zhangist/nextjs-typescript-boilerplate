bcd7bf3bef47f7c4cbcba1d9e70bd437bb47a8ad
zhangist <zhangist@163.com>
2018-01-30 15:19:24 +0800

    remove mui

--- src/pages/mui.tsx
+++ src/pages/mui.tsx
@@ -1,7 +1,5 @@
-import Modal from 'material-ui/Modal'
 import * as React from 'react'
 import AppMenu from '../components/app/Menu'
-import Button from '../components/mui/Button'
 
 export default class MUI extends React.Component<any, any> {
   public state = {
@@ -14,10 +12,6 @@ export default class MUI extends React.Component<any, any> {
         <AppMenu />
         <h3>Hint: These are just some custom examples, and more implementations depend on your creativity.</h3>
         <h3>Modal:</h3>
-        <div>
-          <Button onClick={() => this.setState({openModal: true})}>Button</Button>
-          <Modal open={this.state.openModal}><div>Modal</div></Modal>
-        </div>
       </div>
     )
   }
